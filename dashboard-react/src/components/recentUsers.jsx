cb6a7a323a8f29a9b6eee92bcdc14b33a96c6636
Giovanni <g.buono@esis-italia.com>
2026-05-06 11:57:10 +0200

    User filters

--- dashboard-react/src/components/recentUsers.jsx
+++ dashboard-react/src/components/recentUsers.jsx
@@ -1,22 +1,23 @@
 import { useEffect, useState } from "react"
-import fetchUser, { fetchFilterNames, fetchUserFilter } from "../services/requests"
+import fetchUser from "../services/requests"
 import UserModal from "./UserModal"
-import { Button, Form, InputGroup } from "react-bootstrap"
+import { Button, Form } from "react-bootstrap"
 import { Link } from "react-router-dom"
 
 export default function RecentUsers(props) {
     const [userList, setUserList] = useState([])
+    const [filter, setFilter] = useState("default")
     const [selectedUser, setSelectedUser] = useState(null)
     const [showModal, setShowModal] = useState(false)
-    const [allUsers, setAllUsers] = useState([])
-    const [filterAge, setFilterAge] = useState(0)
     const [message, setMessage] = useState('')
     const [isNew, setIsNew] = useState(false) //isNew
 
     async function getUser(maxUser) {
         const user = await fetchUser(maxUser)
+        console.log(maxUser)
+        // console.log(user.map(item => item.id))
+        // console.log(user.length)
         setUserList(user)
-        setAllUsers(user)
     }
 
     useEffect(() => {
@@ -33,34 +34,6 @@ export default function RecentUsers(props) {
         console.log(user)
     }
 
-    function filterPlus(filterName, value) {
-        if (value === 'default') {
-            setUserList(allUsers)
-            return
-        }
-
-        if (filterName === 'age') {
-            const filtered = allUsers.filter(user => user.age == value)
-
-            if (filtered.length === 0) {
-                setUserList(allUsers)
-            } else {
-                setUserList(filtered)
-            }
-            return
-        }
-        
-        fetchUserFilter(filterName, value).then((res) => {
-            setUserList(res)
-        })
-    }
-    function filterNames(value) {
-        console.log(value)
-        fetchFilterNames(value).then((res) => {
-            setUserList(res)
-        })
-    }
-
     function addButton() {
         setShowModal(true)
         setMessage('Aggiungi cliente')
@@ -68,8 +41,15 @@ export default function RecentUsers(props) {
     }
 
     function renderUser() {
-
-        return (userList.map(item => {
+        return (userList.filter(itemFilter => {
+            if (filter == "default") {
+                return true
+            }
+            if (filter === itemFilter.gender) {
+                return true
+            }
+            return false
+        }).map(item => {
             // console.log(item)
             return (
                 <div className="row-list" key={item.id + item.firstName}>
@@ -84,11 +64,9 @@ export default function RecentUsers(props) {
                         <span>€ 1.200</span>
                     </div>
                     <Button variant="outline-primary" className="modify-btn" onClick={() => editButton(item)}>Modifica</Button>
-                    {!props.inPage &&
-                        <Button variant="outline-primary" className="filter-btn " onClick={() => props.onSelectUser(item)}>Filtra</Button>
-                    }
+                    <Button variant="outline-primary" className="filter-btn " onClick={() => props.onSelectUser(item)}>Filtra</Button> {/* custom-btn */}
                     <nav>
-                        <Link to={`/user/${item.id}`}><Button variant="outline-primary" className="filter-btn ">Details</Button></Link>
+                        <Link to={`/user/${item.id}`}><Button variant="outline-primary" className="filter-btn "><i className="fa fa-info" aria-hidden="true"></i></Button></Link>
                     </nav>
                 </div>
             )
@@ -133,54 +111,17 @@ export default function RecentUsers(props) {
                     </div>
                     <button className="custom-btn" id="add-clients" onClick={addButton}>Aggiungi cliente </button>
                     <div className="client-list" id="client-list-id">
-                        {props.inPage &&
-                            <>
-                                <InputGroup className="mb-3">
-                                    <InputGroup.Text id="basic-addon1"><i className="fa fa-search" aria-hidden="true"></i></InputGroup.Text>
-                                    <Form.Control
-                                        placeholder="Cerca nome dell'utente"
-                                        aria-label="Username"
-                                        aria-describedby="basic-addon1"
-                                        onChange={(e) => filterNames(e.target.value)}
-                                    />
-                                </InputGroup>
-                                <div className='d-flex p-2 gap-3'>
-                                    <Form.Select className="w-25 h-25" aria-label="Default select example" defaultValue="default"
-                                        onChange={(e) => {
-                                            const value = e.target.value
-                                            filterPlus('gender', value)
-                                        }}>
-                                        <option value="default">Genere</option>
-                                        <option value="male">Maschio</option>
-                                        <option value="female">Femmina</option>
-                                    </Form.Select>
-                                    <Form.Select className="w-25 h-25" aria-label="Default select example" defaultValue="default"
-                                        onChange={(e) => {
-                                            const value = e.target.value
-                                            filterPlus('role', value)
-                                        }}>
-                                        <option value="default">Ruolo</option>
-                                        <option value="admin">admin</option>
-                                        <option value="moderator">moderatore</option>
-                                        <option value="user">utente</option>
-                                    </Form.Select>
-                                    <Form.Group className="mb-3">
-                                        <Form.Label>Età - {filterAge}</Form.Label>
-                                        <Form.Range max={140} defaultValue={filterAge} onChange={(e) => {
-                                            const value = e.target.value
-                                            setFilterAge(value)
-                                            filterPlus('age', value)
-                                        }} />
-                                    </Form.Group>
-                                </div>
-
-                            </>
-                        }
+                        <Form.Select aria-label="Default select example" defaultValue={filter}
+                            onChange={(e) => setFilter(e.target.value)}>
+                            <option value="default">Genere</option>
+                            <option value="male">Maschio</option>
+                            <option value="female">Femmina</option>
+                        </Form.Select>
                         {renderUser()}
 
                     </div>
                 </div>
-            </div >
+            </div>
         </>
     )
 }
--- dashboard-react/src/services/requests.js
+++ dashboard-react/src/services/requests.js
@@ -5,40 +5,18 @@ export default function fetchUser(maxUser) {
   })
 }
 
-export function fetchSingleUser(userId) {
+export function fetchSingleUser(userId){
   return fetch(`https://dummyjson.com/users/${userId ? userId : 1}`).then(res => res.json());
 }
 
-export function fetchCarts(userId = null, maxCarts) {
+export function fetchCarts(userId = null,maxCarts) {
   return fetch(`https://dummyjson.com/carts?limit=${maxCarts ? maxCarts : 10}&userId=${userId}`).then(res => res.json()).then(cartResponse => {
     return cartResponse.carts
 
   })
 }
 
-export function fetchFilterNames(value) {
-  return fetch(`https://dummyjson.com/users/search?q=${value}`).then(res => res.json()).then(userResponse => {
-    return userResponse.users
-  })
-}
-
-export function fetchProductsCategory(value) {
-  return fetch(`https://dummyjson.com/products/category/${value}`)
-    .then(res => res.json())
-    .then(productResponse => {
-      return productResponse.products
-    })
-}
-
-export function fetchUserFilter(key, value) {
-  return fetch(`https://dummyjson.com/users/filter?key=${key}&value=${value}`)
-    .then(res => res.json()).then(userResponse => {
-      return userResponse.users
-
-    })
-}
-
-export function fetchProducts(userId = null, maxProducts) {
+export function fetchProducts(userId = null,maxProducts) {
   return fetch(`https://dummyjson.com/products?limit=${maxProducts ? maxProducts : 10}&userId=${userId}`).then(res => res.json()).then(productResponse => {
     return productResponse.products
 
