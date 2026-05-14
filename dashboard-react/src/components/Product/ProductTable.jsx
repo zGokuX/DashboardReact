2920cbef6d2fe61f9ee9a99fc390ad82381d18f0
Andrei Ceban <a.ceban@esis-italia.com>
2026-05-14 16:26:54 +0200

    animation

--- dashboard-react/src/components/Product/ProductTable.jsx
+++ dashboard-react/src/components/Product/ProductTable.jsx
@@ -14,6 +14,7 @@ export default function ProductsTable(props) {
     const totalProducts = useSelector(selectProductsTotal)
     const [activeProductId, setActiveProductId] = useState(null)
     const [copiedImage, setCopiedImage] = useState(null)
+    const [copiedImageCoords, setCopiedImageCoords] = useState(null)
     const [showToast, setShowToast] = useState(false)
     const [animate, setAnimate] = useState(false)
     const [showImage, setShowImage] = useState(false)
@@ -39,21 +40,28 @@ export default function ProductsTable(props) {
 
     }, [pagination]);
 
-    function handleAddToCart(item) {
+    function handleAddToCart(item, e) {
         const isActive = activeProductId === item.id
 
         if (isActive) {
             setActiveProductId(null)
             setCopiedImage(null)
+            setCopiedImageCoords(null)
             setShowImage(false)
             return
         }
+
+        const coords = e.currentTarget.getBoundingClientRect()
 
         setAnimate(false)
         setShowImage(false)
 
         setTimeout(() => {
             setCopiedImage(item.thumbnail)
+            if (coords) {
+                console.log(coords);
+                setCopiedImageCoords(coords)
+            }
             setActiveProductId(item.id)
             setShowImage(true)
 
@@ -82,121 +90,133 @@ export default function ProductsTable(props) {
         }, 1500)
     }
     return (
-        <>
-            {showImage && (
-                <img
-                    src={copiedImage}
-                    className={`movement ${animate ? "active" : ""}`}
-                    width="50"
-                    alt="copied"
-                />
-            )}
-            {showModal &&
-                <>
-                    <ProductModal
-                        show={showModal}
-                        onHide={() => setShowModal(false)}
-                        product={selectProduct}
-                        inPage={props.inPage}
-                    />
-                </>
-            }
-            <table className="card-table invoices-table" id="table-products">
-                <thead>
-                    <tr className="table-header">
-                        <th>Id prodotto</th>
-                        <th>Immagine prodotto</th>
-                        <th>Nome prodotto</th>
-                        <th>Prezzo</th>
-                        <th>Sconto</th>
-                        {!props.inUser &&
-                            <th>{props.modalMode && props.isCarts ? 'Quantità' : 'Categoria'}</th>
-                        }
-
-
-                        {!props.isCarts && !props.inUser &&
-                            <th>Disponibilità</th>
-                        }
-
-
-                        {!props.isCarts && !props.modalMode &&
-
-                            <th></th>
-                        }
-                        {
-                            props.modalMode && props.showMoreOption && (
-                                <>
-                                    <th>Descrizione prodotto</th>
-                                    <th>Valutazioni</th>
-                                </>
-                            )
-                        }
-                    </tr>
-                </thead>
-                <tbody id="bodyTable">
-                    {props.productList?.map((item) => {
-                        return (
-
-                            <tr key={item.id}>
-                                <td>{item.id}</td>
-                                <td><img style={{ display: "flex" }} width="50px" src={item.thumbnail} alt="Products Avatar" /></td>
-                                <td>{item.title}</td>
-                                <td>€ {Math.round(item.price)}</td>
-                                <td>{item.discountPercentage}%</td>
-                                <td>{props.modalMode && props.isCarts ? item.quantity : item.category}</td>
-
-                                {!props.isCarts &&
-                                    <td>{item.availabilityStatus}</td>
-                                }
-
-
-
-                                {
-                                    !props.isCarts && props.showMoreOption ? (
-                                        <>
-                                            <td>{item.description}</td>
-                                            <td>{item.reviews?.map((reviewItem, index) => {
-                                                return <p key={reviewItem.id ?? index}>Rating: {reviewItem.rating} Comment: {reviewItem.comment}</p>
-                                            })}</td>
-
-                                        </>) : (
-                                        <>
-
-                                        </>
-                                    )
-                                }
-                                {!props.isCarts && !props.modalMode && !props.inUser &&
-                                    <td className="d-flex gap-2">
-                                        <Button variant="outline-primary" onClick={() => detailsProductButton(item)}>
-                                            See more
-                                        </Button>
-                                        {props.inPage &&
-                                            < Button variant="outline-primary" onClick={() => handleAddToCart(item)}>
-                                                <BagPlusFill />
-                                            </Button>
-                                        }
-
-                                    </td>
-                                }
-                            </tr>
-
-                        )
-                    })}
-                </tbody>
-            </table >
-            <NotificationAddToCart
-                setShowToast={setShowToast}
-                showToast={showToast}
+      <>
+        {showImage && (
+          <img
+            src={copiedImage}
+            className={`movement ${animate ? 'active' : ''}`}
+            width='50'
+            style={{
+              top: copiedImageCoords?.top
+                ? copiedImageCoords.top + 'px'
+                : '0px',
+              right: copiedImageCoords?.right
+                ? (window.innerWidth - copiedImageCoords.right) + 'px'
+                : '0px',
+            }}
+            alt='copied'
+          />
+        )}
+        {showModal && (
+          <>
+            <ProductModal
+              show={showModal}
+              onHide={() => setShowModal(false)}
+              product={selectProduct}
+              inPage={props.inPage}
             />
-            {
-                props.inPage && (
-                    <PaginationPage
-                        setPagination={setPagination}
-                        pagination={pagination}
-                        totalUsers={totalProducts}
+          </>
+        )}
+        <table className='card-table invoices-table' id='table-products'>
+          <thead>
+            <tr className='table-header'>
+              <th>Id prodotto</th>
+              <th>Immagine prodotto</th>
+              <th>Nome prodotto</th>
+              <th>Prezzo</th>
+              <th>Sconto</th>
+              {!props.inUser && (
+                <th>
+                  {props.modalMode && props.isCarts ? 'Quantità' : 'Categoria'}
+                </th>
+              )}
+
+              {!props.isCarts && !props.inUser && <th>Disponibilità</th>}
+
+              {!props.isCarts && !props.modalMode && <th></th>}
+              {props.modalMode && props.showMoreOption && (
+                <>
+                  <th>Descrizione prodotto</th>
+                  <th>Valutazioni</th>
+                </>
+              )}
+            </tr>
+          </thead>
+          <tbody id='bodyTable'>
+            {props.productList?.map(item => {
+              return (
+                <tr key={item.id}>
+                  <td>{item.id}</td>
+                  <td>
+                    <img
+                      style={{ display: 'flex' }}
+                      width='50px'
+                      src={item.thumbnail}
+                      alt='Products Avatar'
                     />
-                )
-            }
-        </>
+                  </td>
+                  <td>{item.title}</td>
+                  <td>€ {Math.round(item.price)}</td>
+                  <td>{item.discountPercentage}%</td>
+                  <td>
+                    {props.modalMode && props.isCarts
+                      ? item.quantity
+                      : item.category}
+                  </td>
+
+                  {!props.isCarts && <td>{item.availabilityStatus}</td>}
+
+                  {!props.isCarts && props.showMoreOption ? (
+                    <>
+                      <td>{item.description}</td>
+                      <td>
+                        {item.reviews?.map((reviewItem, index) => {
+                          return (
+                            <p key={reviewItem.id ?? index}>
+                              Rating: {reviewItem.rating} Comment:{' '}
+                              {reviewItem.comment}
+                            </p>
+                          )
+                        })}
+                      </td>
+                    </>
+                  ) : (
+                    <></>
+                  )}
+                  {!props.isCarts && !props.modalMode && !props.inUser && (
+                    <td className='d-flex gap-2'>
+                      <Button
+                        variant='outline-primary'
+                        onClick={() => detailsProductButton(item)}
+                      >
+                        See more
+                      </Button>
+                      {props.inPage && (
+                        <Button
+                          variant='outline-primary'
+                          onClick={e => handleAddToCart(item, e)}
+                        >
+                          <BagPlusFill />
+                        </Button>
+                      )}
+                    </td>
+                  )}
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+        <NotificationAddToCart
+          setShowToast={setShowToast}
+          showToast={showToast}
+        />
+        {props.inPage && (
+          <PaginationPage
+            setPagination={setPagination}
+            pagination={pagination}
+            totalUsers={totalProducts}
+          />
+        )}
+      </>
     )
 }
