50a2ebbeca20d4ee3910e739a6a5e6260f2df594
Giovanni <gbuono814@gmail.com>
2026-05-22 10:25:32 +0200

    improving deploy #4

--- dashboard-react/vite.config.js
+++ dashboard-react/vite.config.js
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
-export default defineConfig({
-  plugins: [react()],
-
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: command === 'serve'
+      ? '/'
+      : '/DashboardReact/',
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+  }
 })
