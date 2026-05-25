74f7ef21f26c4587e9db87636a7ab3beae1fbdd6
Andrei Ceban <a.ceban@esis-italia.com>
2026-05-25 15:50:25 +0200

    hashroute, deploy command

--- dashboard-react/src/main.jsx
+++ dashboard-react/src/main.jsx
@@ -6,13 +6,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.jsx'
 import { Provider } from 'react-redux';
 import store from './store/store';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
