58c0b8c55af01f39071705dbedff2d1de577b5fc
Giovanni <gbuono814@gmail.com>
2026-06-08 12:51:38 +0200

    Fixed test

--- dashboard-react/src/test/Navbar.test.jsx
+++ dashboard-react/src/test/Navbar.test.jsx
@@ -23,41 +23,4 @@ describe('NavBarProfile', () => {
         expect(screen.getByText('Profile config')).toBeTruthy() 
         expect(screen.getByText('Optional config')).toBeTruthy()
     });
-    //   it('calls onSave when button is clicked', async () => {
-    // const props = {
-    //   "show": true,
-    //   "onHide": "onHide() {}",
-    //   "product": {
-    //     "id": 1,
-    //     "title": "Essence Mascara Lash Princess",
-    //     "description": "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.",
-    //     "category": "beauty",
-    //     "price": 9.99,
-    //     "discountPercentage": 10.48,
-    //     "rating": 2.56,
-    //     "stock": 99,
-    //     "tags": "[\"beauty\", \"mascara\"]",
-    //     "brand": "Essence",
-    //     "sku": "BEA-ESS-ESS-001",
-    //     "weight": 4,
-    //     "dimensions": "{depth: 22.99, height: 13.08, width: 15.14}",
-    //     "warrantyInformation": "1 week warranty",
-    //     "shippingInformation": "Ships in 3-5 business days",
-    //     "availabilityStatus": "In Stock",
-    //     "reviews": "[{…}, {…}, {…}]",
-    //     "returnPolicy": "No return policy",
-    //     "minimumOrderQuantity": 48,
-    //     "meta": "{barcode: \"5784719087687\", createdAt: \"2025-04-30T0…}",
-    //     "images": "[\"https://cdn.dummyjson.com/product-images/beauty/e…]",
-    //     "thumbnail": "https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/thumbnail.webp"
-    //   },
-    //   "inPage": true
-    // }
-    //     const onSave = vi.fn();
-    //     render(<ProductModal { ...props} />);
-    //     await userEvent.click(
-    //       screen.getByRole('button', { name: 'Save' })
-    //     );
-    //     expect(onSave).toHaveBeenCalledTimes(1);
-    //   });
 });
