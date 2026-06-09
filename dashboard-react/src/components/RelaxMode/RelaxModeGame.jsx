03099f99f2b85c44bb05faed1f9affdc55f8df0f
Giovanni <gbuono814@gmail.com>
2026-06-09 18:11:24 +0200

    continue relax mode game

--- dashboard-react/src/components/RelaxMode/RelaxModeGame.jsx
+++ dashboard-react/src/components/RelaxMode/RelaxModeGame.jsx
@@ -1,9 +1,9 @@
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import "@/components/RelaxMode/RelaxModeStyle.scss"
+import "@/components/RelaxMode/RelaxModeStyle.css"
 export default function RelaxMode() {
     return (
-        <div className="container-relax">
+        <div className="container-relax d-flex flex-column gap-4 ">
             <div className="UpBar ps-5 pt-2 pe-3 d-flex justify-content-between text-light fs-4">
                 <div className="money d-flex align-items-center rounded ps-3 pt-2 pe-5">
                     <div className="icon text-success fs-2">
@@ -27,6 +27,37 @@ export default function RelaxMode() {
                             <p className="mb-0">ESCI</p>
                         </Button>
                     </Link>
+                </div>
+            </div>
+            <div className="chiodi-container ps-5 d-flex">
+                <div className="chiodi-card">
+                    <div className="chiodi-title">
+                        <span>CHIODI</span>
+                    </div>
+                    <div className="d-flex justify-content-center chiodi-emoji">📌</div>
+                    <div className="chiodi-level">LIV.25</div>
+                </div>
+                <div className="produzione">
+                    <div className="produzione-title d-flex gap-5">
+                    <span id="text-produzione">PRODUZIONE DI CHIODI</span>
+                    <span id="money-per-sec">512/SEC</span>
+                    </div>
+                    <div className="progress-bar">
+                        <div className="start"></div>
+                    </div>
+
+                    <div className="sell d-flex">
+                        <span style={{fontSize: "3rem"}}>📦</span>
+                        <div className="title ">
+                            <p className="m-0 magazzino">MAGAZZINO CHIODI</p>
+                            <p className="money-text">24.3K</p>
+                        </div>
+
+                        <div className="button-sell d-flex flex-column">
+                            <p className="m-0">VENDI TUTTO</p>
+                            <p>💵24.3K</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
