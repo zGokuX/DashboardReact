a0558c3fbe161ac9c947e476539bddf82f156f4a
Giovanni <gbuono814@gmail.com>
2026-06-09 11:34:24 +0200

    fixing

--- dashboard-react/src/components/RelaxMode/RelaxModeGame.jsx
+++ dashboard-react/src/components/RelaxMode/RelaxModeGame.jsx
@@ -7,7 +7,7 @@ export default function RelaxMode() {
             <div className="UpBar ps-5 pt-2 pe-3 d-flex justify-content-between text-light fs-4">
                 <div className="money d-flex align-items-center rounded ps-3 pt-2 pe-5">
                     <div className="icon text-success fs-2">
-                        <i class="fa-regular fa-money-bill-1 me-2"></i>
+                        <i className="fa-regular fa-money-bill-1 me-2"></i>
                     </div>
                     <div className="textNumber">
                         <span className="fs-3">12.45K</span>
@@ -19,7 +19,7 @@ export default function RelaxMode() {
                     <span className="+d-flex align-items-center rounded"><i className="fa-solid fa-crosshairs"></i></span>
                 </div>
                 <div className="d-flex shop">
-                    <Button className=" shopbtn d-flex align-items-center rounded fs-4">
+                    <Button className="shopbtn d-flex align-items-center rounded fs-4">
                         <span><i className="fa-solid fa-cart-shopping me-2"></i>NEGOZIO</span>
                     </Button >
                     <Link to="/">
